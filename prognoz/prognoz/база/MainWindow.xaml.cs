a2c362b47701a45cffa7eb9f2ae1307aa73fbc7a
j <112891364+jhellsing@users.noreply.github.com>
2022-11-06 22:29:57 +0300

    Add files via upload

---
+++ prognoz/prognoz/база/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+
+namespace Prognoz
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
